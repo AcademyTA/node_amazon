c968ae27e12758f04b27bd53015b6e8e0dc71df1
Jason <jzntam@gmail.com>
2017-06-15 23:04:05 -0700

    Add review delete

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,10 +3,19 @@ const router  = express.Router({ mergeParams: true })
 
 const { Review } = require('../models')
 
+router.delete('/:id', (request, response, next) => {
+  const { id, productId } = request.params;
+
+  Review.findById(id)
+    .then(review => review.destroy())
+    .then(() => response.redirect(`/products/${productId}`))
+    .catch(error => next(error))
+});
+
 router.post('/', (request, response) => {
   const { productId } = request.params
   const { content, rating } = request.body
-console.log(request.params)
+
   Review.create({
     ProductId: productId,
     content: content,
